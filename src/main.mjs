dba29dd00d438f60cc488e2ac91032f0e2b9b5bf
SherkeyXD <57581480+SherkeyXD@users.noreply.github.com>
2025-02-21 23:48:03 +0800

    build: change back to npm

--- src/main.mjs
+++ src/main.mjs
@@ -1,13 +1,16 @@
 import { createApp } from "vue";
+
 import App from "./App.vue";
 import router from "./router/router.mjs";
 import pinia from "./store/pinia.mjs";
 import { init } from "./utils/initApp.mjs";
 import lazy from "./utils/lazy.mjs";
+
 import "./style.css";
 import "normalize.css";
 import "./assets/css/common.css";
 import "./assets/css/fonts.css";
+
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
